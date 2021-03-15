da8d26d9a3ea58c9e61f9b5c0f661649c216eff7
Dood <E014392@aoins.com>
2021-03-15 18:17:22 -0400

    initial

---
+++ Program.fs
@@ -0,0 +1,19 @@
+open System
+open FSharp.Data.Sql
+
+let [<Literal>] dbVendor = Common.DatabaseProviderTypes.MSSQLSERVER_DYNAMIC
+let [<Literal>] connString = "Server=server;Database=db;Trusted_Connection=True;"
+let [<Literal>] resolutionPath  = "bin/Debug/net5.0/"
+
+type sql = SqlDataProvider<dbVendor, connString, IndividualsAmount=1000, UseOptionTypes=true, ResolutionPath=resolutionPath>
+
+
+// Define a function to construct a message to print
+let from whom =
+    sprintf "from %s" whom
+
+[<EntryPoint>]
+let main argv =
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
